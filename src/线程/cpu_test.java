b2cb34af291490ea7394cfd9b3d3afefcd158c66
AlexShao1116 <111716991+AlexShao1116@users.noreply.github.com>
2024-12-28 21:26:43 -0500

    Delete src directory

--- src/线程/cpu_test.java
+++
@@ -1,9 +0,0 @@
-package 线程;
-
-public class cpu_test {
-    public static void main(String[] args) {
-        Runtime runtime = Runtime.getRuntime();
-        int cpuNums = runtime.availableProcessors();
-        System.out.println(cpuNums);
-    }
-}
